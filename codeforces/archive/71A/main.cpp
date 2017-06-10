ea0f894493580f8e6ac5f9f1ee8ee23769bb0b8b
Medvedev Stas <medvedevsa97@gmail.com>
2017-06-10 17:11:58 +0300

    "71A" Second try

--- codeforces/archive/71A/main.cpp
+++ codeforces/archive/71A/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 int main()
 {
     unsigned short n;
     cin >> n;
-    cout << n;
+    string s[n];
+    for(int i=0; i<n ; i++)
+        cin >> s[i];
+    for(int i=0; i<n ; i++)
+    {
+        if(s[i].length()>=10)
+            cout << s[i][0] <<s[i].length()-2 << s[i][s[i].length()-1]<<'\n';
+        else    
+            cout << s[i];
+            
+    }
     return 0;
 }
