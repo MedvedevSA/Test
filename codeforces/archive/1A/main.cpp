cc0b12f834fd985123dc242323d2572a6b005d5b
Medvedev Stas <medvedevsa97@gmail.com>
2017-06-10 15:13:06 +0300

    cout -> type long

--- codeforces/archive/1A/main.cpp
+++ codeforces/archive/1A/main.cpp
@@ -5,7 +5,8 @@ int main()
 {
     unsigned int n,m,a;
     cin >> n >> m >> a;
-    cout << ((n/a)+(n%a?1:0))*((m/a)+(m%a?1:0));
+    cout << n <<":-:"<<m<<":-:"<<a<<'\n';
+    cout << long((n/a)+(n%a?1:0))*((m/a)+(m%a?1:0));
     return 0;
 
 }
