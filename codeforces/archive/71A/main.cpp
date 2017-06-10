9d4930785b84a9337addbcb51462b4e5f9f9be24
Medvedev Stas <medvedevsa97@gmail.com>
2017-06-10 17:16:22 +0300

    "71A" Sintax Error, I`m stupid

--- codeforces/archive/71A/main.cpp
+++ codeforces/archive/71A/main.cpp
@@ -11,7 +11,7 @@ int main()
         cin >> s[i];
     for(int i=0; i<n ; i++)
     {
-        if(s[i].length()=10)
+        if(s[i].length()<10)
             cout << s[i][0] <<s[i].length()-2 << s[i][s[i].length()-1]<<'\n';
         else    
             cout << s[i]<<'\n';
